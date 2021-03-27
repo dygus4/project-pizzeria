cfa20992784e473cdaa20c312e91723f9c5c0794
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-03-27 19:52:20 +0100

    Add processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -117,10 +117,61 @@
     initOrderForm(){
       const thisProduct = this;
       console.log(this.initOrderForm);
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
     }
+
     processOrder(){
       const thisProduct = this;
-      console.log(this.processOrder);
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+        // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          //Jeśli   (istnieje parametr paramId w formData) oraz  (istnieje parametr paramId w formData który zawiera on optionId) to
+          
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            //jeśli jest niedomyślna a jest wybrana to dodaj koszt do ceny.
+            if (!option.default){
+              price = price + option.price;
+            }
+            //jeśli nie jest wybrana, a jest domyślna, to musimy odjąć cene
+          }else if (option.default){
+            price = price - option.price;
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
 
   }
