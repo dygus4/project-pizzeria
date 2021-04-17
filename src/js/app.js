17fe6b152e8b83f452a80a55fa26698203a9ff50
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-17 17:53:15 +0200

    Add Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,45 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 
 const app = {
   initPages: function(){
     const thisApp = this;
 
-    thisApp.pages = document.querySelector(select.containerOf.pages).childern;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    console.log(thisApp.pages);
-    thisApp.activatePage(thisApp.pages[0].id);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+   
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      } 
+    }
 
     
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        //run thisApp.activatePage with that id
+        thisApp.activatePage(id);
+
+        //change URL hash
+        window.location.hash = '#/' + id;
+      });
+    }  
   },
   activatePage: function(pageId){
     const thisApp = this;
@@ -34,6 +61,12 @@ const app = {
     //add class 'active' to matching links, remove from non-matching
 
   },
+  initBooking: function(){
+    const thisApp = this;
+
+    const element = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(element);
+  } ,
 
   initMenu: function(){
 
@@ -88,6 +121,7 @@ const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
     //thisApp.initMenu();
   },
 };
