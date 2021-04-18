19c46b3c8b685ac9e73cd5dd3aaa9345b6d0aa9a
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-18 17:48:32 +0200

    Add BaseWidget

--- src/js/app.js
+++ src/js/app.js
@@ -90,13 +90,13 @@ const app = {
         return rawResponce.json();
       })
       .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
+        //console.log('parsedResponse', parsedResponse);
         //save parsedResponce at thisApp.data.products
         thisApp.data.products = parsedResponse;
         //execute initMenu method
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
+    //console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
   initCart: function(){
     const thisApp = this;
@@ -116,7 +116,7 @@ const app = {
     //console.log('*** App starting ***');
     //console.log('thisApp:', thisApp);
     //console.log('classNames:', classNames);
-    console.log('settings:', settings);
+    //console.log('settings:', settings);
     //console.log('templates:', templates);
     thisApp.initPages();
     thisApp.initData();
