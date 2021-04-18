579a6fe793ac1d5d72a30892a6ad7479eb1b8ba1
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-18 23:58:18 +0200

    Add getData - part1

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {templates, select} from '../settings.js';
+import {templates, select, settings} from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from '../components/AmountWidget.js';
 import DatePicker from '../components/DatePicker.js';
 import HourPicker from '../components/HourPicker.js';
@@ -9,6 +10,50 @@ class Booking {
         
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+    //console.log('getData params', params);
+
+    const urls = {
+      booking:          settings.db.url + '/' + settings.db.booking 
+                                        + '?' + params.booking.join(' & ') ,
+      eventsCurrent:    settings.db.url + '/' + settings.db.event  
+                                        + '?' + params.eventsCurrent.join(' & ') ,
+      eventsRepeat:     settings.db.url + '/' + settings.db.event   
+                                        + '?' + params.eventsRepeat.join(' & ') ,
+    };
+    //console.log('getData urls', urls);
+
+
+    fetch(urls.booking)
+      .then(function(bookingsResponse){
+        return bookingsResponse.json();
+      })
+      .then(function(bookings){
+        console.log(bookings);
+      });
   }
  
   
