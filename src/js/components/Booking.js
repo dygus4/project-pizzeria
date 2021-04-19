a39e8df56d346da9c5e24e048c2cca1cb9f142af
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-04-19 14:13:08 +0200

    Get data from API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -19,7 +19,7 @@ class Booking {
     const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
     const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
 
-    const params = {
+    const params ={
       booking: [
         startDateParam,
         endDateParam,
@@ -28,31 +28,46 @@ class Booking {
         settings.db.notRepeatParam,
         startDateParam,
         endDateParam,
+
       ],
       eventsRepeat: [
         settings.db.repeatParam,
         endDateParam,
       ],
     };
-    //console.log('getData params', params);
+
+    //console.log('getData.params', params);
 
     const urls = {
-      booking:          settings.db.url + '/' + settings.db.booking 
-                                        + '?' + params.booking.join(' & ') ,
-      eventsCurrent:    settings.db.url + '/' + settings.db.event  
-                                        + '?' + params.eventsCurrent.join(' & ') ,
-      eventsRepeat:     settings.db.url + '/' + settings.db.event   
-                                        + '?' + params.eventsRepeat.join(' & ') ,
+      booking:       settings.db.url + '/' + settings.db.booking
+                                     + '?' + params.booking.join('&'),
+      eventsCurrent:  settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:   settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsRepeat.join('&'),
     };
-    //console.log('getData urls', urls);
+    //console.log('get data urls', urls);
 
-
-    fetch(urls.booking)
-      .then(function(bookingsResponse){
-        return bookingsResponse.json();
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
       })
-      .then(function(bookings){
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
         console.log(bookings);
+        console.log(eventsCurrent); 
+        console.log(eventsRepeat);
+        
       });
   }
  
